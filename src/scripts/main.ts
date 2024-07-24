f6dc26e4d4bb24244ec233905e68bc0a5aecef97
Kalash Shrestha <69349296+kalexrt@users.noreply.github.com>
2024-07-25 00:04:16 +0545

    login page

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,7 @@
 import { Router } from "../router";
+import '../styles/style.css';
 
 document.addEventListener("DOMContentLoaded", async () => {
-
 
   Router.init();
   window.addEventListener("hashchange", () => Router.loadContent());
