22aea198996864f288272ccd967468b6d3cd6d57
Kalash Shrestha <69349296+kalexrt@users.noreply.github.com>
2024-08-03 12:34:48 +0545

    add dockerization

--- src/scripts/book.script.ts
+++ src/scripts/book.script.ts
@@ -2,11 +2,16 @@ import { BookApi } from "../api/book.api";
 
 export class BookActions {
   static async init(id: string) {
+    const notifyBtn = document.getElementById("notifyBtn") as HTMLButtonElement;
     const data = await BookApi.getBookById(id);
     this.updatePageContent(data);
+    // Event listeners initiallization
+    notifyBtn.addEventListener("click", this.handleNotifyMe);
   }
 
   static updatePageContent(data: any) {
+    const notifyDiv = document.getElementById("notify-div") as HTMLDivElement;
+    const accessToken = localStorage.getItem("accessToken") as string;
     const authors = Array.isArray(data.authors)
       ? data.authors.join(", ")
       : data.authors;
@@ -31,6 +36,13 @@ export class BookActions {
     (document.getElementById("total-copies") as HTMLSpanElement).textContent = `${data.total_copies}`;
     (document.getElementById("available-copies") as HTMLSpanElement).textContent = `${data.available_copies}`;
     (document.getElementById("total-reviews") as HTMLSpanElement).textContent = `${data.total_reviews}`;
+
+    if (data.available_copies == 0 && accessToken) {
+      notifyDiv.classList.remove("hidden");
+    }
   }
   
+  static async handleNotifyMe(){
+
+  }
 }
